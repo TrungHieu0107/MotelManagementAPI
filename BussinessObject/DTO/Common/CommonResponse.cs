693d55380e96a72b16c8c398077b8ac49579b24a
KienPham <masterkien1284@gmail.com>
2023-02-17 23:49:49 +0700

    Add common response

---
+++ BussinessObject/DTO/Common/CommonResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject.DTO.Common
+{
+    public class CommonResponse
+    {
+        public object Data { get; set; }
+        public Pagination Pagination { get; set; }
+        public string Message { get; set; }
+
+    }
+}
