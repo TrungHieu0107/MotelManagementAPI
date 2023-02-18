6b4c47b40c0c0458c31044f794ab916deb8992d2
Trung Hiếu <ntrunghieu1701@gmail.com>
2023-02-19 00:15:13 +0700

    Change type invoiceDTO to nullable

--- BussinessObject/DTO/InvoiceDTO.cs
+++ BussinessObject/DTO/InvoiceDTO.cs
@@ -11,16 +11,16 @@ namespace BussinessObject.DTO
     {
         public long Id { get; set; }
         public int ElectricityConsumptionStart { get; set; }
-        public int ElectricityConsumptionEnd { get; set; }
+        public int? ElectricityConsumptionEnd { get; set; }
         public long ElectricityCostId { get; set; }
         public int WaterConsumptionStart { get; set; }
-        public int WaterConsumptionEnd { get; set; }
+        public int? WaterConsumptionEnd { get; set; }
         public long WaterCostId { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime PaidDate { get; set; }
+        public DateTime? PaidDate { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public DateTime ExpiredDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? ExpiredDate { get; set; }
         public long RoomId { get; set; }
         public long ResidentId { get; set; }
         public InvoiceStatus Status { get; set; }
