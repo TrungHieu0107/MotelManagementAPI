d1e6ea108b11daf81d3e68ba964e44047ceddbdd
Trung Hiếu <ntrunghieu1701@gmail.com>
2023-03-07 22:23:26 +0700

    Add filter option room

---
+++ MotelManagementWebAppUI/Models/FilterRoomOption.cs
@@ -0,0 +1,21 @@
+﻿using BussinessObject.DTO.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MotelManagementWebAppUI.Models
+{
+    public class FilterRoomOption
+    {
+        [Display(Name = "Mã phòng")]
+        public string roomCode { get; set; }
+        [Display(Name = "Giá nhỏ nhất")]
+        public long? minFee { get; set; }
+        [Display(Name = "Giá lớn nhất")]
+        public long? maxFee { get; set; }
+        [Display(Name = "Trạng thái")]
+        public int? status { get; set; }
+        [Display(Name = "Ngày áp dụng")]
+        public DateTime? appliedDateAfter { get; set; }
+        public Pagination? pagination { get; set; }
+    }
+}
