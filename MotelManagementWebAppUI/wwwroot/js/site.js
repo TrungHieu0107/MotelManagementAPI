2db3a72d598286944ee5dcd60b84cac7dd91edc6
Trung Hiếu <ntrunghieu1701@gmail.com>
2023-03-10 13:53:48 +0700

    Add modal cofirm

--- MotelManagementWebAppUI/wwwroot/js/site.js
+++ MotelManagementWebAppUI/wwwroot/js/site.js
@@ -12,7 +12,7 @@ function showConfirm(body, func) {
         '<div class="modal-content">' +
         '<div class="modal-header">' +
         '<h5 class="modal-title">Nhắc nhở</h5>' +
-        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close" id="confirm-no-button">' +
         '<span aria-hidden="true">&times;</span>' +
         '</button>' +
         '</div>' +
@@ -21,7 +21,7 @@ function showConfirm(body, func) {
         '<div id="confirm-body">' +
         '</div>' +
         '<div class="modal-footer">' +
-        '<button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Không</button>' +
+        '<button type="button" class="btn btn-outline-secondary" data-dismiss="modal" id="confirm-no-button">Không</button>' +
         '<button type="button" class="btn btn-outline-warning" id="confirm-yes-button">Có</button>' +
         '</div>' +
         '</div>' +
@@ -39,13 +39,23 @@ function showConfirm(body, func) {
             draggable: true,
             resizable: true,
         });
-        $('#confirm-body').append(body);
 
+        $('#confirm-modal').on('hidden.bs.modal', () => {
+            $('#confirm-modal').remove();
+            $('.modal-backdrop').remove();
+        });
+
+        $('#confirm-body').append(body);
+        $('#confirm-modal').modal('toggle');
         $('#confirm-yes-button').on('click', function () {
             func();
 
             $('#confirm-modal').remove();
             $('.modal-backdrop').remove();
+        });
+
+        $('#confirm-no-button').on('click', function () {
+            $('#confirm-modal').modal('toggle');
         });
 
     })
