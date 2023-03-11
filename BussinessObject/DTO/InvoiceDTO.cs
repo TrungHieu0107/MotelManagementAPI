4733a43b907d13646caffcd2a27a0ba9f46713e1
KienPham <masterkien1284@gmail.com>
2023-03-11 19:52:25 +0700

    update invoice in checkout room

--- BussinessObject/DTO/InvoiceDTO.cs
+++ BussinessObject/DTO/InvoiceDTO.cs
@@ -7,10 +7,14 @@ namespace BussinessObject.DTO
     public class InvoiceDTO
     {
         public long Id { get; set; }
+        [Display(Name = "Số điện bắt đầu tính hóa đơn")]
         public int ElectricityConsumptionStart { get; set; }
+        [Display(Name = "Số điện kết thúc tính hóa đơn")]
         public int? ElectricityConsumptionEnd { get; set; }
         public long ElectricityCostId { get; set; }
+        [Display(Name = "Số nước bắt đầu tính hóa đơn")]
         public int WaterConsumptionStart { get; set; }
+        [Display(Name = "Số điện kết thúc tính hóa đơn")]
         public int? WaterConsumptionEnd { get; set; }
         public long WaterCostId { get; set; }
 
